fde60dcde0ec3c19383d90e499f638fabc067850
DonChaka <44255641+DonChaka@users.noreply.github.com>
2023-01-13 00:21:37 +0100

    Changed classification to new rects system

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import cv2
 
+from utils.classifier import load_dataset, get_trained_model
 from utils.globals import *
-from utils.contour import add_rects_on_image, match_rects_to_images
+from utils.contour import match_rects_to_images, add_rects_on_image
+from utils.prediction import add_rects_with_pred_on_image
 
 COLOR_SETS = True
+
+dataset = load_dataset(path=DATASET_PATH, hog_params=HOG_PARAMS, slice_image_shape=OUTPUT_SLICE_IMAGE_SHAPE)
+model = get_trained_model(
+        x_train_data=dataset[0],
+        y_train_data=dataset[1],
+        model_class=CLASSIFIER_MODEL_CLASS,
+        model_params=CLASSIFIER_PARAMS,
+        model_search=CLASSIFIER_SEARCH_BEST_MODEL,
+        search_params=CLASSIFIER_SEARCH_BEST_MODEL_PARAMS
+    )
 
 for video_name in ('example_day', 'example_night'):
     cap = cv2.VideoCapture(join('videos', f'{video_name}.mp4'))
@@ -17,7 +29,17 @@ for video_name in ('example_day', 'example_night'):
 
         if ret:
             rects = match_rects_to_images(image=frame)
-            add_rects_on_image(img=frame, all_rects=rects)
+            if len(rects):
+                add_rects_with_pred_on_image(
+                    image=frame,
+                    rects_data=rects,
+                    model=model,
+                    slice_image_shape=OUTPUT_SLICE_IMAGE_SHAPE,
+                    rect_thickness=SIGN_RECTANGLE_THICKNESS,
+                    font_size=FONT_SIZE,
+                    font_thickness=FONT_THICKNESS,
+                    hog_params=HOG_PARAMS
+                )
 
             cv2.imshow('Frame', frame)
 
